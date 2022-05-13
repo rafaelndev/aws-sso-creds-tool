6f8a399824aaa3b2af43ee3674acaf32fd576119
rafaelndev <rafaeln.dev@gmail.com>
2022-05-13 13:42:47 -0300

    fixed error logging

--- params.js
+++ params.js
@@ -2,7 +2,7 @@
 const os = require("os")
 
 module.exports = Object.freeze({
-    startUrl: process.env.SSO_URL ?? error('please set the sso default url'),
+    startUrl: process.env.SSO_URL ?? console.error('please set the sso default url'),
     awsCredentialsPath: process.env.AWS_CREDENTIALS_PATH ?? `${os.homedir()}/.aws/credentials`,
     useAccountId: process.env.USE_ACCOUNT_ID === 'true',
     sso_accounts: process.env.ACCOUNTS?.split(','),
